ac12882303aa4aea32dc9b4a0ef5ceb3534b25a4
t0rx <yan@VAIOFit15.localdomain>
2018-04-15 18:25:08 +0100

    First version of library.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'friendly-schedule',
+  packages = ['friendly-schedule'],
+  version = '0.1',
+  description = 'Library for parsing human-friendly schedule strings',
+  author = 't0rx',
+  author_email = 't0rxon@googlemail.com',
+  url = 'https://github.com/t0rx/friendly-schedule',
+  download_url = 'https://github.com/t0rx/friendly-schedule/archive/0.1.tar.gz',
+  keywords = ['schedule'],
+  classifiers = [],
+)
+
